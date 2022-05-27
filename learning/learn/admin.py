3f1b50281e65ff1ed5ac8e735e0d487034d202f1
Leena K Babu <leenakbabu.24@gmail.com>
2022-05-27 18:57:46 +0530

    first

---
+++ learning/learn/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import *
+admin.site.register(Registration)
+admin.site.register(Subject)
+admin.site.register(Enrollment)
+admin.site.register(Learning_progress)
+admin.site.register(Feedback)
+admin.site.register(Messages)
+admin.site.register(Exam)
+admin.site.register(Exam_results)
+admin.site.register(Blogs)
+admin.site.register(Requests)
---
+++ learning/learn/decorators.py
@@ -0,0 +1,48 @@
+from . models import *
+from django.http import HttpResponseRedirect
+from django.contrib import messages
+
+def logged_inn2(function):
+    def _function(request,*args,**kwargs):
+        m = Registration.objects.get(user = request.user)
+        if m.User_role != 'admin':
+            try:
+                del request.session['logg']
+                messages.success(request, 'Please login as admin')
+                return HttpResponseRedirect('home')
+            except:
+                messages.success(request, 'Please login as admin')
+                return HttpResponseRedirect('home')
+        else:
+            return function(request, *args, **kwargs)
+    return _function
+
+def logged_inn3(function):
+    def _function(request,*args,**kwargs):
+        m = Registration.objects.get(user = request.user)
+        if m.User_role != 'admin':
+            try:
+                del request.session['logg']
+                messages.success(request, 'Please login as student')
+                return HttpResponseRedirect('home')
+            except:
+                messages.success(request, 'Please login as student')
+                return HttpResponseRedirect('home')
+        else:
+            return function(request, *args, **kwargs)
+    return _function
+
+def logged_inn4(function):
+    def _function(request,*args,**kwargs):
+        m = Registration.objects.get(user = request.user)
+        if m.User_role != 'admin':
+            try:
+                del request.session['logg']
+                messages.success(request, 'Please login as teacher')
+                return HttpResponseRedirect('home')
+            except:
+                messages.success(request, 'Please login as teacher')
+                return HttpResponseRedirect('home')
+        else:
+            return function(request, *args, **kwargs)
+    return _function
